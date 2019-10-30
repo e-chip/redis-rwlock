5f7ec2285d031f0e4ac5466c1f0f10d0b665c6c2
Egor Gorlin <egor.gorlin@gmail.com>
2019-10-30 20:02:15 +0200

    Update example. Remove example from readme file.

--- example/main.go
+++ example/main.go
@@ -5,60 +5,84 @@ import (
 	"sync"
 	"time"
 
-	"github.com/e-chip/redis-rwlock"
+	rwlock "github.com/e-chip/redis-rwlock"
 	"github.com/go-redis/redis"
 )
 
-func writeSharedData(locker rwlock.Locker, wg *sync.WaitGroup, sharedData *int) {
-	for {
-		err := locker.Write(func() {
-			fmt.Printf("Writing...\n")
-			time.Sleep(500 * time.Millisecond)
-			(*sharedData)++
-			fmt.Printf("Write: %d\n", *sharedData)
-		})
-		if err != nil {
-			fmt.Printf("Writing error: %v\n", err)
-			if err != rwlock.ErrTimeout {
-				break
-			}
-		}
-		time.Sleep(2 * time.Second)
-	}
-	wg.Done()
+const (
+	readersCount    = 10
+	writeIterations = 5
+	writeDuration   = 500 * time.Millisecond
+	writeInterval   = 2 * time.Second
+)
+
+type example struct {
+	locker rwlock.Locker
+	wg     sync.WaitGroup
+	doneC  chan struct{}
 }
 
-func readSharedData(locker rwlock.Locker, wg *sync.WaitGroup, sharedData *int) {
-	for {
-		err := locker.Read(func() {
-			fmt.Printf("Read: %d\n", *sharedData)
-		})
-		if err != nil {
-			fmt.Printf("Read error: %v\n", err)
-			if err != rwlock.ErrTimeout {
-				break
+func (e *example) WriteSharedData(sharedData *int) {
+	e.wg.Add(1)
+	go func() {
+		for i := 0; i < writeIterations; i++ {
+			err := e.locker.Write(func() {
+				fmt.Printf("Writing...\n")
+				time.Sleep(writeDuration)
+				(*sharedData)++
+				fmt.Printf("Write: %d\n", *sharedData)
+			})
+			if err != nil {
+				fmt.Printf("Writing error: %v\n", err)
+			}
+			time.Sleep(writeInterval)
+		}
+		close(e.doneC)
+		e.wg.Done()
+	}()
+}
+
+func (e *example) ReadSharedData(sharedData *int) {
+	e.wg.Add(1)
+	go func() {
+		for {
+			select {
+			case <-e.doneC:
+				e.wg.Done()
+				return
+			default:
+				err := e.locker.Read(func() {
+					fmt.Printf("Read: %d\n", *sharedData)
+				})
+				if err != nil {
+					fmt.Printf("Read error: %v\n", err)
+				}
 			}
 		}
-	}
-	wg.Done()
+	}()
+}
+
+func (e *example) Wait() {
+	e.wg.Wait()
 }
 
 func main() {
 	var (
-		wg          sync.WaitGroup
 		sharedData  = 0
 		redisClient = redis.NewClient(&redis.Options{
 			Network: "tcp",
 			Addr:    "localhost:6379",
 			DB:      9,
 		})
-		locker = rwlock.Make(redisClient, "GLOBAL_LOCK", "READER_COUNT", "WRITER_INTENT", &rwlock.Options{})
+		example = example{
+			locker: rwlock.Make(redisClient, "GLOBAL_LOCK", "READER_COUNT", "WRITER_INTENT", &rwlock.Options{}),
+			doneC:  make(chan struct{}),
+		}
 	)
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go readSharedData(locker, &wg, &sharedData)
+	defer redisClient.Close()
+	for i := 0; i < readersCount; i++ {
+		example.ReadSharedData(&sharedData)
 	}
-	wg.Add(1)
-	go writeSharedData(locker, &wg, &sharedData)
-	wg.Wait()
+	example.WriteSharedData(&sharedData)
+	example.Wait()
 }
