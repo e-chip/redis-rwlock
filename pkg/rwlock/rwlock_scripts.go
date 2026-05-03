638221dec03ce30e1fad9d809142768f16db9d57
Egor Gorlin <egor.gorlin@gmail.com>
2026-05-03 15:13:14 +0300

    Move and embed Lua lock scripts

--- pkg/rwlock/rwlock_scripts.go
+++ pkg/rwlock/rwlock_scripts.go
@@ -1,77 +1,18 @@
 package rwlock
 
-import (
-	"github.com/go-redis/redis"
-)
+import _ "embed"
 
-var (
-	acquireReadLock = redis.NewScript(readLockScript)
-	releaseReadLock = redis.NewScript(readUnlockScript)
+//go:embed lua/read-lock.lua
+var readLockScript string
 
-	refreshLock = redis.NewScript(lockRefreshScript)
+//go:embed lua/read-unlock.lua
+var readUnlockScript string
 
-	acquireWriteLock = redis.NewScript(writeLockScript)
-	releaseWriteLock = redis.NewScript(writeUnlockScript)
-)
+//go:embed lua/lock-refresh.lua
+var lockRefreshScript string
 
-// KEYS = [GLOB_LOCK_KEY, READ_LOCK_REF_COUNT, WRITER_LOCK_INTENT]
-// ARGV = [TOKEN, EXPIRATION_TIMEOUT, WRITER_PREFERRING]
-const readLockScript = `
-if ARGV[3] ~= 0 and redis.call("EXISTS", KEYS[3]) == 1 then
-	return 0
-else
-	if redis.call("INCR", KEYS[2]) == 1  then
-		if redis.call("SET", KEYS[1], ARGV[1], "PX", ARGV[2], "NX") then
-            return 1
-        else
-            redis.call("DECR", KEYS[2])
-            return 0
-        end
-    else
-        return 1
-    end
-end`
+//go:embed lua/write-lock.lua
+var writeLockScript string
 
-// KEYS = [GLOB_LOCK_KEY, READ_LOCK_REF_COUNT]
-// ARGV = [TOKEN]
-const readUnlockScript = `
-if redis.call("DECR", KEYS[2]) == 0 then
-    if redis.call("GET", KEYS[1]) == ARGV[1] then
-        redis.call("DEL", KEYS[1], KEYS[2])
-        return 1
-    else
-        return 0
-    end
-else
-    return 1
-end`
-
-// KEYS = [GLOB_LOCK_KEY]
-// ARGV = [TOKEN, EXPIRATION_TIMEOUT]
-const lockRefreshScript = `
-if redis.call("GET", KEYS[1]) == ARGV[1] then
-    return redis.call("PEXPIRE", KEYS[1], ARGV[2])
-else
-    return 0
-end`
-
-// KEYS = [GLOB_LOCK_KEY, READ_LOCK_REF_COUNT, WRITER_LOCK_INTENT]
-// ARGV = [TOKEN, EXPIRATION_TIMEOUT]
-const writeLockScript = `
-redis.call("SET", KEYS[3], 1, "PX", ARGV[2])
-if redis.call("SET", KEYS[1], ARGV[1], "PX", ARGV[2], "NX") then
-    redis.call("DEL", KEYS[2], KEYS[3])
-    return 1
-else
-    return 0
-end`
-
-// KEYS = [GLOB_LOCK_KEY]
-// ARGV = [TOKEN]
-const writeUnlockScript = `
-if redis.call("GET", KEYS[1]) == ARGV[1] then
-    redis.call("DEL", KEYS[1])
-    return 1
-else
-    return 0
-end`
+//go:embed lua/write-unlock.lua
+var writeUnlockScript string
